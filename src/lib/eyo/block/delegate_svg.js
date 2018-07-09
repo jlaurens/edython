59774b3e0b25f65dab61d76371d5a6c4d37357b7
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-07-09 08:39:05 +0200

    fix an infinite loop

--- src/lib/eyo/block/delegate_svg.js
+++ src/lib/eyo/block/delegate_svg.js
@@ -1115,7 +1115,7 @@ eYo.DelegateSvg.prototype.chainTiles = function () {
         if (next.isVisible()) {
           next.eyo.tilePrevious = tile
           tile.eyo.tileNext = next
-          tile = next
+          next = tile
         }
       }
       tile.eyo.tileNext = null
