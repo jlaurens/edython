fe85e5d026c4b49a3917bcecb2e8bc519f968b6d
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-07-12 20:33:31 +0200

    some unexpected null value

--- src/lib/eyo/blockly/bugfix.js
+++ src/lib/eyo/blockly/bugfix.js
@@ -47,3 +47,24 @@ Blockly.BlockDragger.prototype.endBlockDrag = function (e, currentDragDeltaXY) {
   }
   Blockly.Events.setGroup(false)
 }
+
+/**
+ * Handle a mouse move or touch move event.
+ * @param {!Event} e A mouse move or touch move event.
+ * @package
+ */
+Blockly.Gesture.prototype.handleMove = function(e) {
+  this.updateFromEvent_(e);
+  if (this.isDraggingWorkspace_) {
+    this.workspaceDragger_.drag(this.currentDragDeltaXY_);
+  } else if (this.isDraggingBlock_) {
+    this.blockDragger_ && this.blockDragger_.dragBlock(this.mostRecentEvent_,
+        this.currentDragDeltaXY_); // sometimes it fails
+  } else if (this.isDraggingBubble_) {
+    this.bubbleDragger_ && this.bubbleDragger_.dragBubble(this.mostRecentEvent_,
+        this.currentDragDeltaXY_);
+  }
+  e.preventDefault();
+  e.stopPropagation();
+};
+
