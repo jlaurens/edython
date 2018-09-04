b6c95264dda3f3431fe536f2e33629c14e5e683d
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-09-04 13:52:59 +0200

    in progress: unary and binary

--- src/lib/eyo/block/delegate_svg_operator.js
+++ src/lib/eyo/block/delegate_svg_operator.js
@@ -21,6 +21,209 @@ goog.require('eYo.DelegateSvg.Expr')
  * Abstract class.
  * For edython.
  */
+eYo.DelegateSvg.Expr.makeSubclass('binary', {
+  data: {
+    operator: { // only one field with that key
+      synchronize: true
+    }
+  },
+  slots: {
+    lhs: {
+      order: 1,
+      hole_value: 'name',
+      check: /** @suppress {globalThis} */ function (type) {
+        return eYo.DelegateSvg.Expr.binary.getOperatorModelForType(type).lhs
+      }
+    },
+    rhs: {
+      order: 2,
+      fields: {
+        operator: ''
+      },
+      hole_value: 'name',
+      check: /** @suppress {globalThis} */ function (type) {
+        return eYo.DelegateSvg.Expr.binary.getOperatorModel(type).rhs
+      }
+    }
+  }
+})
+
+/**
+ * Consolidate the type of the block.
+ * Unstable state.
+ * For edython.
+ */
+eYo.DelegateSvg.Expr.binary.consolidateType = function (block) {
+  block.type = this.getTypeForOperator(this.data.operator.get())
+  eYo.DelegateSvg.Expr.binary.superClass_.consolidateType.call(this, block)
+}
+
+/**
+ * Get the operator model.
+ * For edython.
+ * @return a dictionary
+ */
+eYo.DelegateSvg.Expr.binary.getOperatorModelForType = function (type) {
+  return {
+    [eYo.T3.Expr.m_expr]: {
+      lhs: eYo.T3.Expr.Check.m_expr_all,
+      rhs: eYo.T3.Expr.Check.u_expr_all
+    },
+    [eYo.T3.Expr.a_expr]: {
+      lhs: eYo.T3.Expr.Check.a_expr_all,
+      rhs: eYo.T3.Expr.Check.m_expr_all
+    },
+    [eYo.T3.Expr.number_comparison]: {
+      lhs: eYo.T3.Expr.Check.comparison,
+      rhs: eYo.T3.Expr.Check.comparison
+    },
+    [eYo.T3.Expr.or_test]: {
+      lhs: eYo.T3.Expr.Check.or_test_all,
+      rhs: eYo.T3.Expr.Check.and_test_all
+    },
+    [eYo.T3.Expr.and_test]: {
+      lhs: eYo.T3.Expr.Check.and_test_all,
+      rhs: eYo.T3.Expr.Check.not_test_all
+    },
+    [eYo.T3.Expr.object_comparison]: {
+      lhs: eYo.T3.Expr.Check.comparison,
+      rhs: eYo.T3.Expr.Check.comparison
+    },
+    [eYo.T3.Expr.power]: {
+      lhs: eYo.T3.Expr.Check.await_or_primary,
+      rhs: eYo.T3.Expr.Check.u_expr_all
+    },
+    [eYo.T3.Expr.shift_expr]: {
+      lhs: eYo.T3.Expr.Check.shift_expr_all,
+      rhs: eYo.T3.Expr.Check.a_expr_all
+    },
+    [eYo.T3.Expr.and_expr]: {
+      lhs: eYo.T3.Expr.Check.and_expr_all,
+      rhs: eYo.T3.Expr.Check.shift_expr_all
+    },
+    [eYo.T3.Expr.xor_expr]: {
+      lhs: eYo.T3.Expr.Check.xor_expr_all,
+      rhs: eYo.T3.Expr.Check.and_expr_all
+    },
+    [eYo.T3.Expr.or_expr]: {
+      lhs: eYo.T3.Expr.Check.or_expr_all,
+      rhs: eYo.T3.Expr.Check.xor_expr_all
+    }
+  } [type]
+}
+
+/**
+ * Get the operator model.
+ * For edython.
+ * @return a dictionary
+ */
+eYo.DelegateSvg.Expr.binary.getTypeForOperator = function (op) {
+  if (['*', '//', '/', '%', '@'].indexOf(op) >= 0) {
+    return eYo.T3.Expr.m_expr
+  }
+  if (['+', '-'].indexOf(op) >= 0) {
+    return eYo.T3.Expr.a_expr
+  }
+  if (['<', '>', '==', '>=', '<=', '!='].indexOf(op) >= 0) {
+    return eYo.T3.Expr.number_comparison
+  }
+  if ('or' === op) {
+    return eYo.T3.Expr.or_test
+  }
+  if ('and' === op) {
+    return eYo.T3.Expr.and_test
+  }
+  if (['is', 'is not', 'in', 'not in'].indexOf(op) >= 0) {
+    return eYo.T3.Expr.object_comparison
+  }
+  if ('**' === op) {
+    return eYo.T3.Expr.power
+  }
+  if (['<<', '>>'].indexOf(op) >= 0) {
+    return eYo.T3.Expr.shift_expr
+  }
+  if ('&' === op) {
+    return eYo.T3.Expr.and_expr
+  }
+  if ('^' === op) {
+    return eYo.T3.Expr.xor_expr
+  }
+  if ('|' === op) {
+    return eYo.T3.Expr.or_expr
+  }
+}
+
+/**
+ * Class for a DelegateSvg, unary op ... block.
+ * u_expr.
+ * For edython.
+ */
+eYo.DelegateSvg.Expr.makeSubclass('unary', {
+  data: {
+    operator: {
+      main: true,
+      all: ['-', '+', '~', 'not']
+    }
+  },
+  slots: {
+    rhs: {
+      check: /** @suppress {globalThis} */ function () {
+        return this.owner.getOperatorModel().rhs
+      }
+    }
+  }
+})
+
+/**
+ * Get the operator model.
+ * For edython.
+ * @return a dictionary
+ */
+eYo.DelegateSvg.Expr.unary.getOperatorModel = function () {
+  var op = this.data.operator.get()
+  if (['-', '+', '~'].indexOf(op) >= 0) {
+    return {
+      type: eYo.T3.Expr.u_expr,
+      rhs: eYo.T3.Expr.Check.u_expr_all
+    }
+  }
+  if ('not' === op) {
+    return {
+      type: eYo.T3.Expr.not_test,
+      rhs: eYo.T3.Expr.Check.not_test_all
+    }
+  }
+}
+
+/**
+ * Consolidate the type of the block.
+ * Unstable state.
+ * For edython.
+ */
+eYo.DelegateSvg.Expr.unary.consolidateType = function (block) {
+  eYo.DelegateSvg.Expr.unary.superClass_.consolidateType.call(this, block)
+  var m = this.getOperatorModel()
+  block.type = m.type
+}
+
+/**
+ * Consolidate the connections of the block.
+ * State partial mutator (mutates the state to a possibly unconsistent state).
+ * For edython.
+ * @param {!Blockly.Block} block The block.
+ */
+eYo.DelegateSvg.Expr.unary.consolidateConnections = function (block) {
+  eYo.DelegateSvg.Expr.unary.superClass_.consolidateConnections.call(this, block)
+  var m = this.getOperatorModel()
+  block.outputConnection && block.outputConnection.setCheck(m.rhs)
+}
+
+/**
+ * Class for a DelegateSvg, [...] op ... block.
+ * Multiple ops.
+ * Abstract class.
+ * For edython.
+ */
 eYo.DelegateSvg.Expr.makeSubclass('Operator', {
   data: {
     operator: { // only one field with that key
