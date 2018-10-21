36e7cbf45f41f7bbe9c4e1b4182caa9c2568dff0
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-10-21 20:47:09 +0200

    In progress

--- src/lib/eyo/block/delegate_svg_statistics.js
+++ src/lib/eyo/block/delegate_svg_statistics.js
@@ -42,8 +42,69 @@ var F = function(key) {
     data: key
   }
 }
-eYo.FlyoutCategory.basic_statistics__module = 
+
+var F = function (name, title) {
+  var key = 'statistics__'+name
+  title && (eYo.Tooltip.Title[key] = title)
+  return {
+    type: eYo.T3.Expr.call_expr,
+    data: {
+      name: name,
+      holder: 'statistics',
+      dotted: 0
+    },
+    title: key
+  }
+}
+
+eYo.FlyoutCategory.basic_statistics__module = [
+  {
+    type: eYo.T3.Stmt.import_stmt,
+    from_d: 'statistics',
+    variant_d: eYo.Key.FROM_MODULE_IMPORT_STAR,
+    title: 'statistics__import_stmt'
+  },
+  F('mode', 'Mode de l\'argument, une séquence ou un itérateur, en tant que valeur représentative.'),
+  F('mean', 'Moyenne arithmétique de l\'argument, une séquence ou un itérateur.'),
+  F('harmonic_mean', 'Moyenne harmonique de l\'argument, une séquence ou un itérateur.'),
+  F('median', 'Valeur médiane de l\'argument, une séquence ou un itérateur.'),
+  F('median_high', 'Valeur médiane (sans moyenne et par excès) de l\'argument, une séquence ou un itérateur.'),
+  F('median_low', 'Valeur médiane (sans moyenne et par défaut) de l\'argument, une séquence ou un itérateur.'),
+  F('median_grouped', 'Valeur médiane de l\'argument, une séquence ou un itérateur avec répétitions possibles.'),
+  F('pstdev', 'Écart type d\'une population'),
+  F('pvariance', 'Variance d\'une population'),
+  F('stdev', 'Écart type d\'un échantillon d\'une population'),
+  F('variance', 'Variance d\'un échantillon d\'une population'),
+  F('StatisticsError', 'Exception spécifique au module `statistics`')
+]
+
+var F = function (name, title) {
+  var key = 'statistics__'+name
+  title && (eYo.Tooltip.Title[key] = title)
+  return {
+    type: eYo.T3.Expr.call_expr,
+    data: {
+      name: name,
+      holder: 'statistics',
+      dotted: 1
+    },
+    title: key
+  }
+}
+
 eYo.FlyoutCategory.statistics__module = [
+  {
+    type: eYo.T3.Stmt.import_stmt,
+    variant_d: eYo.Key.IMPORT,
+    import_module_s: {
+      slots: {
+        O: {
+          type: eYo.T3.Expr.identifier,
+          data: 'statistics'
+        }
+      }
+    }
+  },
   F('mode'),
   F('mean'),
   F('harmonic_mean'),
