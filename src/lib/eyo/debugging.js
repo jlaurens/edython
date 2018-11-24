d5bc07f988c2dacc69f330738b87bcbef180791e
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-11-24 12:05:12 +0100

    In progress

--- src/lib/eyo/debugging.js
+++ src/lib/eyo/debugging.js
@@ -1,15 +1,2 @@
 // Remove this file when done
 // copy and override functions here.
-
-Blockly.Workspace.prototype.fireChangeListener = function(event) {
-  if (event.recordUndo) {
-    this.undoStack_.push(event);
-    this.redoStack_.length = 0;
-    if (this.undoStack_.length > this.MAX_UNDO) {
-      this.undoStack_.unshift();
-    }
-  }
-  for (var i = 0, func; func = this.listeners_[i]; i++) {
-    func(event);
-  }
-};
