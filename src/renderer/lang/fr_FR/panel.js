1987e28d3b1f12e47f6d8a7c3b2573e5d2e1139d
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-11-28 21:19:45 +0100

    Revert "In progress"
    This reverts commit 0cd415b7909ce6d3a0a26add0d6eb53baf408b09.

--- src/renderer/lang/fr_FR/panel.js
+++
@@ -1,6 +0,0 @@
-export default {
-  should_save: {
-    title: 'Sauvegarder d\'abord ?',
-    content: 'Des changements sont en cours, ils seront définitivement perdus s\'ils ne sont pas sauvegardés.'
-  }
-}
