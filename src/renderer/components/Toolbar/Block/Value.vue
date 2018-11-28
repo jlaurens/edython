66d661927dff82eaf255c1eb256c845e5d409994
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-11-28 20:52:28 +0100

    Revert "Update src/renderer/components/Toolbar/Block/Value.vue"
    This reverts commit e1a460fa4f13c258b0b758cc42378fec613157e9.

--- src/renderer/components/Toolbar/Block/Value.vue
+++ src/renderer/components/Toolbar/Block/Value.vue
@@ -30,12 +30,7 @@
       formatter: {
         type: Function,
         default: function (item) {
-          return item && item.length
-            ? this.$$t(`block.${({
-              '*': 'star',
-              '**': 'two_stars'
-            }[item] || item)}`)
-            : '&nbsp;'
+          return item && item.length ? this.$t('message.' + ({'*': 'star', '**': 'two_stars'}[item] || item)) : '&nbsp;'
         }
       }
     },
