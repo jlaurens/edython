0cd415b7909ce6d3a0a26add0d6eb53baf408b09
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-11-28 20:20:04 +0100

    In progress

---
+++ src/renderer/lang/fr_FR/panel.js
@@ -0,0 +1,6 @@
+export default {
+  should_save: {
+    title: 'Sauvegarder d\'abord ?',
+    content: 'Des changements sont en cours, ils seront définitivement perdus s\'ils ne sont pas sauvegardés.'
+  }
+}
