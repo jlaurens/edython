4d8d94074004d629935d17e82284142d5edb0110
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-11-30 13:06:58 +0100

    Update src/renderer/components/Toolbar/Block/Unary.vue

--- src/renderer/components/Toolbar/Block/Unary.vue
+++ src/renderer/components/Toolbar/Block/Unary.vue
@@ -1,9 +1,9 @@
 <template>
-    <b-dropdown id="block-unary-operator" class="eyo-dropdown item text  eyo-with-slotholder" variant="outline-secondary">
+    <b-dd id="block-unary-operator" class="eyo-dropdown item text  eyo-with-slotholder" variant="outline-secondary">
       <template slot="button-content"><span class="eyo-code eyo-content text" v-html="formatter(operator)"></span></template>
-      <b-dropdown-item-button v-for="item in operators" v-on:click="operator = item" :key="item" class="block-unary-operator eyo-code" v-html="formatter(item)"></b-dropdown-item-button>
-      </b-dropdown-item-button>
-    </b-dropdown>
+      <b-dd-item-button v-for="item in operators" v-on:click="operator = item" :key="item" class="block-unary-operator eyo-code" v-html="formatter(item)"></b-dd-item-button>
+      </b-dd-item-button>
+    </b-dd>
   </template>
   
   <script>
