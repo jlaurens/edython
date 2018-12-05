ac8d4547ae64a05f862b7be9dd0596ec53ed25cd
jlaurens <jerome.laurens@u-bourgogne.fr>
2018-12-05 14:21:28 +0100

    Update src/lib/eyo/core/ui.js

--- src/lib/eyo/core/ui.js
+++ src/lib/eyo/core/ui.js
@@ -245,46 +245,245 @@ eYo.setup.register(-1, function () {
 })
 
 eYo.setup.register(function () {
-  eYo.Style.insertCssRuleAt('.blocklyMainBackground { stroke-width: 5;stroke: none; }')
+  eYo.Style.insertCssRuleAt(
+    `.blocklyMainBackground {
+      stroke-width: 5;
+      stroke: none;
+    }
+  `)
 }, 'OVERRIDE')
 
-eYo.setup.register(function () {
-  eYo.Style.insertCssRuleAt('.eyo-block .blocklyText, .eyo-var, .eyo-label, .eyo-code, .eyo-code-reserved, .eyo-code-builtin, .eyo-code-comment, .eyo-code-placeholder, .eyo-sharp-group{ ' + eYo.Font.style + ';}')
-  eYo.Style.insertCssRuleAt('.eyo-error.eyo-path-selected, .eyo-error.eyo-path-shape, .eyo-error.eyo-path-contour, .eyo-error.eyo-path-inner {stroke: ' + eYo.Style.Path.Error.colour + ';}')
-  var w = eYo.Style.Path.Selected.width
-  console.warn('PATH WIDTH:', w, ';stroke-width: ' + w + 'px;fill: none;}')
-  eYo.Style.insertCssRuleAt('.eyo-path-selected{stroke: ' + eYo.Style.Path.Selected.colour + ';stroke-width: ' + eYo.Style.Path.Selected.width + 'px;fill: none;}')
-  eYo.Style.insertCssRuleAt('.eyo-select .eyo-path-contour, .eyo-select .eyo-path-inner, .eyo-select.eyo-inner.eyo-expr .eyo-path-contour,  .eyo-select .eyo-inner.eyo-expr .eyo-path-contour, .eyo-select .eyo-inner.eyo-expr .eyo-path-inner {stroke: ' + eYo.Style.Path.Selected.colour + ';}')
-  eYo.Style.insertCssRuleAt('.eyo-select .eyo-path-contour.eyo-error, .eyo-select .eyo-path-inner.eyo-error{stroke: ' + eYo.Style.Path.Error.colour + ';}')
-  eYo.Style.insertCssRuleAt('.eyo-checkbox-icon-rect{stroke: ' + eYo.Style.Path.colour + ';stroke-width: ' + eYo.Style.Path.width + 'px;fill: white;}')
-  eYo.Style.insertCssRuleAt('.eyo-locked.eyo-path-contour, .eyo-locked.eyo-path-inner, .eyo-locked.eyo-path-shape, .eyo-locked.eyo-edit, .eyo-locked.eyo-edit{display: none}')
-  eYo.Style.insertCssRuleAt('.eyo-path-shape{stroke: none;fill: white;fill-opacity:0.85}')
-  eYo.Style.insertCssRuleAt('.eyo-inner .eyo-path-shape{stroke: none;fill-opacity:0}')
-  eYo.Style.insertCssRuleAt('.eyo-stmt.eyo-inner .eyo-path-shape{stroke: none;fill-opacity:0.85}')
-  eYo.Style.insertCssRuleAt('.eyo-path-contour, .eyo-path-inner, .eyo-path-collapsed {stroke: ' + eYo.Style.Path.colour + ';stroke-width: ' + eYo.Style.Path.width + 'px;fill: none;pointer-events: all;stroke-linejoin="round"; }')
-  eYo.Style.insertCssRuleAt('.eyo-path-selected {pointer-events: none; }')
-  eYo.Style.insertCssRuleAt('.eyo-inner.eyo-expr .eyo-path-contour, .eyo-inner.eyo-expr .eyo-path-collapsed {stroke: ' + eYo.Style.Path.inner_colour + ';}')
-  eYo.Style.insertCssRuleAt('.eyo-none {stroke:none;fill:none;}')
-  eYo.Style.insertCssRuleAt('.eyo-edit {stroke: none;stroke-width: ' + eYo.Style.Edit.width + 'px;fill: none;}')
-  eYo.Style.insertCssRuleAt('.eyo-select>g>g>.eyo-edit, .eyo-select>g>.eyo-edit {stroke: ' + eYo.Style.Path.colour + ';}')
-  eYo.Style.insertCssRuleAt('rect.eyo-editing, .eyo-locked .eyo-edit ,.eyo-select>g.eyo-locked>g>.eyo-edit, .eyo-select>g.eyo-locked>.eyo-edit {stroke: none;}')
-  eYo.Style.insertCssRuleAt('.eyo-path-dotted{stroke: ' + eYo.Style.Path.colour + ';stroke-width: ' + (eYo.Style.Path.width * 1.5) + 'px;stroke-linecap:round;stroke-dasharray:0 ' + eYo.Font.space / 2 + ';}')
-  eYo.Style.insertCssRuleAt('.eyo-no-path{display:none;}', 5)
-  eYo.Style.insertCssRuleAt('.eyo-code-emph {font-weight: bold;}')
-  eYo.Style.insertCssRuleAt('.eyo-code-reserved, .eyo-code-builtin, .eyo-sharp-group {font-weight:bold!important;color: rgba(0, 84, 147, 0.75)!important;fill: rgba(0, 84, 147, 0.75)!important;}')
-  eYo.Style.insertCssRuleAt('.eyo-code-builtin {font-weight:bold;color: rgba(60, 0, 145, 0.75);fill: rgba(60, 0, 145, 0.75);}')
-  eYo.Style.insertCssRuleAt('.eyo-menuitem-disabled .eyo-code-reserved {color: rgba(60, 0, 145, 0.3);fill: rgba(0, 84, 147, 0.3);}')
-  eYo.Style.insertCssRuleAt('.eyo-menuitem-disabled .eyo-code-builtin {font-weight:bold;color: rgba(60, 0, 145, 0.3);fill: rgba(60, 0, 145, 0.3);}')
-  eYo.Style.insertCssRuleAt('.eyo-code-placeholder, .eyo-code-comment {font-style: oblique;}')
-  eYo.Style.insertCssRuleAt('.eyo-start>g>.eyo-code-comment {font-style: normal;font-weight: bold;}')
-  eYo.Style.insertCssRuleAt('.eyo-start>text.eyo-code-reserved.eyo-label {opacity:0;}')
-  eYo.Style.insertCssRuleAt('.eyo-code-placeholder {fill: rgba(0, 0, 0, 0.4);}')
-  eYo.Style.insertCssRuleAt('input.eyo-code-error {color: red;}')
-  eYo.Style.insertCssRuleAt('text.eyo-code-error {fill: red;}')
-  eYo.Style.insertCssRuleAt('text.eyo-code-comment {fill: rgba(42, 132, 45, 0.8);}')
-  eYo.Style.insertCssRuleAt('.eyo-code-disabled {color: #ccc;}')
-  eYo.Style.insertCssRuleAt('.blocklyHighlightedConnectionPath{stroke: ' + eYo.Style.Path.Selected.colour + ';stroke-width: ' + eYo.Style.Path.Selected.width + 'px;fill: none;}')
-  eYo.Style.insertCssRuleAt('.blocklyHighlightedConnectionPathH{fill: ' + eYo.Style.Path.Selected.colour + ';stroke: none;}')
+eYo.setup.register(() => {
+  eYo.Style.insertCssRuleAt(
+    `.eyo-block .blocklyText,
+    .eyo-var,
+    .eyo-label,
+    .eyo-code,
+    .eyo-code-reserved,
+    .eyo-code-builtin,
+    .eyo-code-comment,
+    .eyo-code-placeholder,
+    .eyo-sharp-group{
+      ${eYo.Font.style};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-error.eyo-path-selected,
+    .eyo-error.eyo-path-shape,
+    .eyo-error.eyo-path-contour,
+    .eyo-error.eyo-path-inner {
+      stroke:${eYo.Style.Path.Error.colour};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-path-selected {
+      stroke: ${eYo.Style.Path.Selected.colour};
+      stroke-width: ${eYo.Style.Path.Selected.width}px;
+      fill: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-select .eyo-path-contour,
+    .eyo-select .eyo-path-inner,
+    .eyo-select.eyo-inner.eyo-expr .eyo-path-contour,
+    .eyo-select .eyo-inner.eyo-expr .eyo-path-contour,
+    .eyo-select .eyo-inner.eyo-expr .eyo-path-inner {
+      stroke: ${eYo.Style.Path.Selected.colour};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-select .eyo-path-contour.eyo-error,
+    .eyo-select .eyo-path-inner.eyo-error {
+      stroke: ${eYo.Style.Path.Error.colour};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-checkbox-icon-rect {
+      stroke: ${eYo.Style.Path.colour};
+      stroke-width: ${eYo.Style.Path.width}px;
+      fill: white;
+      }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-locked.eyo-path-contour,
+    .eyo-locked.eyo-path-inner,
+    .eyo-locked.eyo-path-shape,
+    .eyo-locked.eyo-edit,
+    .eyo-locked.eyo-edit{
+      display: none
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-path-shape {
+      stroke: none;
+      fill: white;
+      fill-opacity:0.85
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-inner .eyo-path-shape {
+      stroke: none;
+      fill-opacity:0
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-stmt.eyo-inner .eyo-path-shape {
+      stroke: none;
+      fill-opacity:0.85;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-path-contour,
+    .eyo-path-inner,
+    .eyo-path-collapsed {
+      stroke: ${eYo.Style.Path.colour};
+      stroke-width: ${eYo.Style.Path.width}px;
+      fill: none;
+      pointer-events: all;
+      stroke-linejoin="round";
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-path-selected {
+      pointer-events: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-inner.eyo-expr .eyo-path-contour,
+    .eyo-inner.eyo-expr .eyo-path-collapsed {
+      stroke: ${eYo.Style.Path.inner_colour};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-none {
+      stroke: none;
+      fill: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-edit {
+      stroke: none;
+      stroke-width:${eYo.Style.Edit.width}px;
+      fill: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-select>g>g>.eyo-edit,
+    .eyo-select>g>.eyo-edit {
+      stroke: ${eYo.Style.Path.colour};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `rect.eyo-editing,
+    .eyo-locked .eyo-edit,
+    .eyo-select>g.eyo-locked>g>.eyo-edit,
+    .eyo-select>g.eyo-locked>.eyo-edit {
+      stroke: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-path-dotted {
+      stroke: ${eYo.Style.Path.colour};
+      stroke-width: ${(eYo.Style.Path.width * 1.5)}px;
+      stroke-linecap: round;
+      stroke-dasharray: 0 ${eYo.Font.space / 2};
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-no-path {
+      display: none;
+    }`,
+    5
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-emph {
+      font-weight: bold;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-reserved,
+    .eyo-code-builtin,
+    .eyo-sharp-group {
+      font-weight: bold!important;
+      color: rgba(0, 84, 147, 0.75)!important;
+      fill: rgba(0, 84, 147, 0.75)!important;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-builtin {
+      font-weight: bold;
+      color: rgba(60, 0, 145, 0.75);
+      fill: rgba(60, 0, 145, 0.75);
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-menuitem-disabled .eyo-code-reserved {
+      color: rgba(60, 0, 145, 0.3);
+      fill: rgba(0, 84, 147, 0.3);
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-menuitem-disabled .eyo-code-builtin {
+      font-weight: bold;
+      color: rgba(60, 0, 145, 0.3);
+      fill: rgba(60, 0, 145, 0.3);
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-placeholder,
+    .eyo-code-comment {
+      font-style: oblique;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-start>g>.eyo-code-comment {
+      font-style: normal;
+      font-weight: bold;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-start>text.eyo-code-reserved.eyo-label {
+      opacity:0;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-placeholder {
+      fill: rgba(0, 0, 0, 0.4);
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `input.eyo-code-error {
+      color: red;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `text.eyo-code-error {
+      fill: red;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `text.eyo-code-comment {
+      fill: rgba(42, 132, 45, 0.8);
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.eyo-code-disabled {
+      color: #ccc;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+    `.blocklyHighlightedConnectionPath{
+      stroke: ${eYo.Style.Path.Selected.colour};
+      stroke-width: ${eYo.Style.Path.Selected.width}px;
+      fill: none;
+    }`
+  )
+  eYo.Style.insertCssRuleAt(
+      `.blocklyHighlightedConnectionPathH{
+        fill: ${eYo.Style.Path.Selected.colour};
+        stroke: none;
+    }`
+  )
 }, 'Style')
 
 eYo.Style.MenuIcon = {
