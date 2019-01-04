1318056088c274244d4ef6f30b3b19a63e4a127b
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-01-04 15:34:16 +0100

    Update src/renderer/components/Toolbar/Block/Decorator.vue

--- src/renderer/components/Toolbar/Block/Decorator.vue
+++ src/renderer/components/Toolbar/Block/Decorator.vue
@@ -1,33 +1,44 @@
 <template>
-  <b-btn-toolbar
-    id="block-decorator"
-    key-nav
-    aria-label="Block decorator"
-    justify>
-    <b-btn-group>
-      <b-form-input
-        v-model="name"
-        type="text"
-        class="item text w-30rem"
-        :style='{fontFamily: $$.eYo.Font.familyMono}'
-        :title="title"
-        v-tippy ></b-form-input>
-      <b-dd
-        id="block-variant"
-        class="eyo-dropdown"
-        variant="outline-secondary">
-        <b-dd-item-button
-          v-for="choice in choices"
-          v-on:click="choose(choice)"
-          :key="choice"
-          class="block-variant eyo-code" v-html="content(choice)"></b-dd-item-button>
-      </b-dd>    
-      <span
-        v-if="property !== $$.eYo.Key.GETTER"
-        class="eyo-code-reserved btn btn-outline-secondary">.{{property}}</span>
-    </b-btn-group>
-    <comment></comment>
-  </b-btn-toolbar>
+  <b-btn-group>
+    <div
+    class="item text eyo-code-reserved"
+    :style="{fontFamily: $$.eYo.Font.familyMono}"
+    >@</div>
+    <b-dd
+      class="eyo-dropdown"
+      variant="outline-secondary">
+      <b-dd-item-button
+        v-for="choice in mainChoices"
+        v-on:click="mainChooser = choice"
+        :key="choice"
+        class="block-variant eyo-code" v-html="mainContent(choice)"></b-dd-item-button>
+    </b-dd>    
+    <b-form-input
+      v-model="name"
+      type="text"
+      :class="$$class(name)"
+      :style='{fontFamily: $$.eYo.Font.familyMono}'
+      :title="title"
+      v-tippy ></b-form-input>
+    <b-dd
+      class="eyo-dropdown"
+      variant="outline-secondary">
+      <b-dd-item-button
+        v-for="choice in choices"
+        v-on:click="chooser = choice"
+        :key="choice"
+        class="block-variant eyo-code" v-html="content(choice)"></b-dd-item-button>
+    </b-dd>    
+    <div
+      v-if="chooser === $$.eYo.Key.N_ARY"
+      class="item text"
+      v-html="ry"
+    ></div>
+    <div
+      v-else-if="chooser !== $$.eYo.Key.NONE"
+      class="item text eyo-code-reserved"
+    >.{{property}}</div>
+  </b-btn-group>
 </template>
 
 <script>
@@ -40,11 +51,21 @@
       return {
         saved_step: undefined,
         property_: undefined,
-        name_: undefined
+        name_: undefined,
+        mainChooser_: undefined,
+        chooser_: undefined
       }
     },
     components: {
       Comment
+    },
+    props: {
+      slotholder: {
+        type: Function,
+        default: function (item) {
+          return item
+        }
+      }
     },
     computed: {
       ...mapState('Selected', [
@@ -53,41 +74,89 @@
       ...mapGetters('Selected', [
         'eyo'
       ]),
-      name () {
-        this.$$synchronize(this.step)
-        return this.name_
+      ry () {
+        return `(${this.slotholder('eyo-slotholder-inline')}):`
+      },
+      name: {
+        get () {
+          this.$$synchronize(this.step)
+          return this.name_
+        },
+        set (newValue) {
+          this.eyo.name_p = newValue
+        }
       },
       property () {
         this.$$synchronize(this.step)
         return this.property_
       },
+      mainChooser: {
+        get () {
+          this.$$synchronize(this.step)
+          return this.mainChooser_
+        },
+        set (newValue) {
+          this.eyo.mainChooser_p = newValue
+        }
+      },
+      chooser: {
+        get () {
+          this.$$synchronize(this.step)
+          return this.chooser_
+        },
+        set (newValue) {
+          this.eyo.chooser_p = newValue
+        }
+      },
+      mainChoices () {
+        return this.eyo.mainChooser_d.getAll()
+      },
       choices () {
-        return this.eyo.data.chooser.getAll()
+        return this.eyo.chooser_d.getAll()
       },
       title () {
         return this.$$t('block.tooltip.decorator.input')
       }
     },
     methods: {
-      content (choice) {
+      mainContent (choice) {
         if (choice === eYo.Key.NONE) {
-          return '@…'
+          return '…'
         } else if (choice === eYo.Key.N_ARY) {
-          return '@…(…)'
+          return '…'
         } else if (choice === eYo.Key.SETTER) {
-          return '@….setter'
+          return '…'
         } else if (choice === eYo.Key.DELETER) {
-          return '@….deleter'
+          return '…'
         } else {
-          return '@' + choice
+          return choice
         }
       },
-      choose (choice) {
-        this.eyo.chooser_p = choice
+      content (choice) {
+        if (choice === eYo.Key.NONE) {
+          return ''
+        } else if (choice === eYo.Key.N_ARY) {
+          return '(…)'
+        } else if (choice === eYo.Key.SETTER) {
+          return '.setter'
+        } else if (choice === eYo.Key.DELETER) {
+          return '.deleter'
+        } else {
+          return ''
+        }
       },
       $$doSynchronize (eyo) {
         this.property_ = eyo.property_p
         this.name_ = eyo.name_p
+        this.mainChooser_ = eyo.mainChooser_p
+        this.chooser_ = eyo.chooser_p
+      },
+      $$class (name) {
+        return `item text w-24rem${[
+          eYo.Key.STATICMETHOD,
+          eYo.Key.CLASSMETHOD,
+          eYo.Key.PROPERTY
+        ].indexOf(this.mainChooser) < 0 ? '' : ' eyo-code-reserved'}`
       }
     }
   }
