2268085dd041bef821951372cc4841717d68513e
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-01-13 20:07:16 +0100

    Update eyoplugin.js

--- src/renderer/plugin/eyoplugin.js
+++ src/renderer/plugin/eyoplugin.js
@@ -5,20 +5,19 @@ eYoPlugin.test = function () {
 }
 
 eYoPlugin.install = function (Vue, options) {
-  console.error('INSTALLING eYoPlugin')
-  // 1. add global method or property
-  Vue.myGlobalMethod = function () {
-    // some logic ...
-    console.log('THIS IS MY GLOBAL METHOD')
-  }
+  // // 1. add global method or property
+  // Vue.myGlobalMethod = function () {
+  //   // some logic ...
+  //   console.log('THIS IS MY GLOBAL METHOD')
+  // }
 
-  // 2. add a global asset
-  Vue.directive('my-directive', {
-    bind (el, binding, vnode, oldVnode) {
-      // some logic ...
-      console.log('THIS IS MY DIRECTIVE')
-    }
-  })
+  // // 2. add a global asset
+  // Vue.directive('my-directive', {
+  //   bind (el, binding, vnode, oldVnode) {
+  //     // some logic ...
+  //     console.log('THIS IS MY DIRECTIVE')
+  //   }
+  // })
 
   // 3. inject some component options
   Vue.mixin({
