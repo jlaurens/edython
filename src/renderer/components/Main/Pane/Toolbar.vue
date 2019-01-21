8d9fa29a1f483aa77e2fda28cd24379ef2b56591
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-01-21 21:49:42 +0100

    Update Toolbar.vue

--- src/renderer/components/Main/Pane/Toolbar.vue
+++ src/renderer/components/Main/Pane/Toolbar.vue
@@ -187,9 +187,9 @@
             }
           }
           // Problem of synchronization : `this.paneLayout` may not be consistent
-          console.error('NO CFG...', this.where, Ls)
+          // console.error('NO CFG...', this.where, Ls)
         }
-        console.error('For paneLayout', this.paneLayout)
+        // console.error('For paneLayout', this.paneLayout)
         return []
       },
       choices () {
