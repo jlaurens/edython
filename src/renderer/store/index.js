e33a080abb841f9d2599b3f4d768de0120461890
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-01-28 20:07:13 +0100

    Update index.js

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -12,7 +12,7 @@ Vue.use(Vuex)
 // ]
 
 const vuexLocalStorage = new VuexPersist({
-  key: 'vuex', // The key to store the state on in the storage provider.
+  key: 'edython', // The key to store the state on in the storage provider.
   storage: window.localStorage, // or window.sessionStorage or localForage
   // Function that passes the state and returns the state with only the objects you want to store.
   reducer: (state) => ({
