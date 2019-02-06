a28ea096317b2f1e2ebde5ba86f75634db14d829
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-02-06 14:33:22 +0100

    Update DocumentController.vue

--- src/renderer/env/web/DocumentController.vue
+++ src/renderer/env/web/DocumentController.vue
@@ -1,13 +1,15 @@
 <template>
-  <div
-    id="document-open"
-    ref="dialog"
-    style="display:none;">
-    <input
-      ref="input"
-      type="file"
-      @change="loadTextFromFile">
-  </div>
+  <b-modal
+    ref="openDialog"
+    :title="$$t('panel.document-open.title')">
+    <b-form-file
+    v-model="file"
+    :state="!!file"
+    :placeholder="$$t('panel.document-open.placeholder')"
+    accept=".eyo"
+    ref="input"
+    ></b-form-file
+  ></b-modal>
 </template>
 <script>
   import {mapState, mapMutations} from 'vuex'
@@ -16,6 +18,7 @@
     name: 'document-controller',
     data: function () {
       return {
+        file: undefined
       }
     },
     computed: {
@@ -25,11 +28,49 @@
     },
     mounted () {
       // console.error('WEB DOCUMENT CONTROLLER MOUNT')
+      this.input = this.$refs.input
+      this.openDialog = this.$refs.openDialog
       this.$$onOnly('document-open', (evt, callback) => { // callback: (path) -> ()
         this.callback = callback
-        eYo.App.Document.shouldSave(() => {
-          this.$refs.input.click()
-        })
+        // var doOpen = () => {
+        //   eYo.App.Document.shouldSave(() => {
+        //     var el = this.input
+        //     if (el.fireEvent) {
+        //       el.fireEvent('onclick')
+        //     } else {
+        //       var evObj = document.createEvent('Events')
+        //       evObj.initEvent('click', true, false)
+        //       el.dispatchEvent(evObj)
+        //     }
+        //     // el.click()
+        //   })
+        // }
+        // for some reason, we cannot safely simulate a click on the input button.
+        // I guess there is an interference with bootstrap
+        // because it does work in more simple situations
+        // Here is what we do instead
+        // localizing the after element in bootstrap is quite a pain.
+        var addRule = (style => {
+          var sheet = document.head.appendChild(style).sheet
+          return function (selector, css) {
+            var propText = typeof css === 'string'
+              ? css
+              : Object.keys(css).map(p => `${p}:${p === 'content' ? `'${css[p]}'` : css[p]}`).join(';')
+            sheet.insertRule(`${selector}{${propText}}`, sheet.cssRules.length)
+          }
+        })(document.createElement('style'))
+        const xx = document.querySelector('.custom-file-label')
+        if (xx) {
+          var content = getComputedStyle(xx, ':after').content
+          var expected = this.$$t('message.browse')
+          if (content !== expected) {
+            addRule('.custom-file-label::after', {
+              content: this.$$t('message.browse')
+            })
+          }
+        }
+        this.input.reset()
+        this.openDialog.show()
       })
       var do_it = (evt, callback) => { // callback: (path) -> ()
         var p = this.path
@@ -66,33 +107,33 @@
     methods: {
       ...mapMutations('Undo', [
         'stageUndo'
-      ]),
-      loadTextFromFile (evt) {
-        console.error('loadTextFromFile')
-        const file = evt.target.files[0]
+      ])
+    },
+    watch: {
+      file (file, oldValue) {
+        this.openDialog.hide()
         if (file) {
-          console.error('loadTextFromFile', file)
-          const reader = new FileReader()
-          reader.onload = e => {
-            console.error('onload loadTextFromFile', file)
-            var result = e.target.result
-            var content = new Uint8Array(result)
-            eYo.App.Document.readDeflate(content, file.name)
-            this.stageUndo()
-            eYo.App.workspace.eyo.resetChangeCount()
-            this.$root.$emit('document-open-complete')
-            eYo.$$.app.$nextTick(() => {
-              eYo.Selected.selectOneBlockOf(eYo.App.workspace.topBlocks_, true)
-              this.$root.$emit('pane-workspace-visible')
-            })
-          }
-          // console.log(file)
-          reader.readAsArrayBuffer(file)
-          this.callback && this.callback()
+          this.$nextTick(() => {
+            const reader = new FileReader()
+            reader.onload = e => {
+              var result = e.target.result
+              var content = new Uint8Array(result)
+              eYo.App.Document.readDeflate(content, file.name)
+              this.stageUndo()
+              eYo.App.workspace.eyo.resetChangeCount()
+              this.$root.$emit('document-open-complete')
+              eYo.$$.app.$nextTick(() => {
+                eYo.Selected.selectOneBlockOf(eYo.App.workspace.topBlocks_, true)
+                this.$root.$emit('pane-workspace-visible')
+              })
+            }
+            // console.log(file)
+            reader.readAsArrayBuffer(file)
+            this.callback && this.callback()
+          })
         } else {
           this.$root.$emit('document-open-abort')
         }
-        this.callback = undefined
       }
     }
   }
