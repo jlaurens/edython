6739037f6c9d1b5f1deef7abe76f7e0b17edd79a
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-02-07 12:14:32 +0100

    Support for escape

--- src/lib/eyo/core/xre.js
+++ src/lib/eyo/core/xre.js
@@ -77,9 +77,9 @@ eYo.Do.readOnlyMixin(eYo.Key, {
 
 eYo.Do.readOnlyMixin(eYo.XRE, {
   [eYo.Key._CHARACTER_SINGLE]: '[\\x20-\\x26\\x28-\\x5B\\x5D-\\uFFFF]|\\\\[\\x0A\\x0D\\x20-\\uFFFF]',
-  [eYo.Key._LONG_CHARACTER_SINGLE]: '[\\x0A\\x0D\\x20-\\x26\\x28-\\x5B\\x5D-\\uFFFF]\\\\[\\x0A\\x0D\\x20-\\uFFFF]',
+  [eYo.Key._LONG_CHARACTER_SINGLE]: '[\\x0A\\x0D\\x20-\\x26\\x28-\\x5B\\x5D-\\uFFFF]|\\\\[\\x0A\\x0D\\x20-\\uFFFF]',
   [eYo.Key._CHARACTER_DOUBLE]: '[\\x20-\\x21\\x23-\\x5B\\x5D-\\uFFFF]|\\\\[\\x0A\\x0D\\x20-\\uFFFF]',
-  [eYo.Key._LONG_CHARACTER_DOUBLE]: '[\\x0A\\x0D\\x20-\\x21\\x23-\\x5B\\x5D-\\uFFFF]\\\\[\\x0A\\x0D\\x20-\\uFFFF]'
+  [eYo.Key._LONG_CHARACTER_DOUBLE]: '[\\x0A\\x0D\\x20-\\x21\\x23-\\x5B\\x5D-\\uFFFF]|\\\\[\\x0A\\x0D\\x20-\\uFFFF]'
 })
 
 eYo.Do.readOnlyMixin(eYo.XRE, {
