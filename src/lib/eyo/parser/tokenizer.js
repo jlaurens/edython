b965fd59ebde99244952df2d46e718170a14613f
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-02-21 18:57:19 +0100

    Update tokenizer.js

--- src/lib/eyo/parser/tokenizer.js
+++ src/lib/eyo/parser/tokenizer.js
@@ -235,6 +235,87 @@ Object.defineProperties(eYo.Scan.prototype, {
     get () {
       return this.paren_stack.length
     }
+  },
+  first: {
+    get () {
+      return this.tokens[0]
+    }
+  },
+  last: {
+    get () {
+      return this.tokens[this.tokens.length - 1]
+    }
+  }
+})
+
+Object.defineProperties(eYo.Token, {
+  NAMES: {
+    get () {
+      return [
+        "ENDMARKER",
+        "NAME",
+        "NUMBER",
+        "STRING",
+        "NEWLINE",
+        "INDENT",
+        "DEDENT",
+        "LPAR",
+        "RPAR",
+        "LSQB",
+        "RSQB",
+        "COLON",
+        "COMMA",
+        "SEMI",
+        "PLUS",
+        "MINUS",
+        "STAR",
+        "SLASH",
+        "VBAR",
+        "AMPER",
+        "LESS",
+        "GREATER",
+        "EQUAL",
+        "DOT",
+        "PERCENT",
+        "LBRACE",
+        "RBRACE",
+        "EQEQUAL",
+        "NOTEQUAL",
+        "LESSEQUAL",
+        "GREATEREQUAL",
+        "TILDE",
+        "CIRCUMFLEX",
+        "LEFTSHIFT",
+        "RIGHTSHIFT",
+        "DOUBLESTAR",
+        "PLUSEQUAL",
+        "MINEQUAL",
+        "STAREQUAL",
+        "SLASHEQUAL",
+        "PERCENTEQUAL",
+        "AMPEREQUAL",
+        "VBAREQUAL",
+        "CIRCUMFLEXEQUAL",
+        "LEFTSHIFTEQUAL",
+        "RIGHTSHIFTEQUAL",
+        "DOUBLESTAREQUAL",
+        "DOUBLESLASH",
+        "DOUBLESLASHEQUAL",
+        "AT",
+        "ATEQUAL",
+        "RARROW",
+        "ELLIPSIS",
+        "COLONEQUAL",
+        "OP",
+        "TYPE_IGNORE",
+        "TYPE_COMMENT",
+        "<ERRORTOKEN>",
+        "<COMMENT>",
+        "<NL>",
+        "<ENCODING>",
+        "<N_TOKENS>",
+      ]
+    }
   }
 })
 
