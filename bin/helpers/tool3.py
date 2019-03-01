da986369f0958cf5b74e53719795b5708abc5109
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-03-01 22:46:00 +0100

    In progress

--- bin/helpers/tool3.py
+++ bin/helpers/tool3.py
@@ -10,6 +10,8 @@ pathRoot = pathlib.Path(__file__).resolve().parent.parent.parent
 pathBuild = pathRoot / 'build' / 'helpers'
 pathBuild.mkdir(parents=True, exist_ok=True)
 
+pathTest = pathRoot / 'test' / 'test.js'
+
 # read the build.sh, change it and save the modified file to the build
 def updateBuild(path_in, path_out, path_deps):
     re_good = re.compile(r"^--js ")
@@ -54,6 +56,59 @@ def updateWeb(path_in, path_out, path_deps):
             return 0
     return 1
 
+def updateTest(path_in, path_out, path_deps):
+    re_start = re.compile(r"^\s*//\s*DYNAMIC DEPS START\s+.*$")
+    re_end = re.compile(r"^\s*//\s*DYNAMIC DEPS END\s+.*$")
+    with path_in.open('r', encoding='utf-8') as f:
+        head = []
+        tail = []
+        fill = head
+        for l in f.readlines():
+            if re_start.match(l):
+                fill.append(l)
+                fill = None
+            elif re_end.match(l):
+                fill = tail
+                fill.append(l)
+            elif fill is not None:
+                fill.append(l)
+        with path_deps.open('r', encoding='utf-8') as f:
+            head.append(f.read())
+            head.extend(tail)
+            path_out.write_text(''.join(head), encoding='utf-8')
+            return 0
+    return 1
+
+def updateWebTests(path_root, path_deps):
+    re_start = re.compile(r"^\s*<\!--\s+DYNAMIC DEPS START\s+-->\s*$")
+    re_end = re.compile(r"^\s*<\!--\s+DYNAMIC DEPS END\s+-->\s*$")
+    global pathRoot
+    files = [x for x in path_root.glob('**/*')
+            if x.is_file()
+            if os.path.basename(x).endswith('.test.html')]
+    for path_in in files:
+        head = []
+        tail = []
+        fill = head
+        with path_in.open('r', encoding='utf-8') as f:
+            for l in f.readlines():
+                if re_start.match(l):
+                    fill.append(l)
+                    fill = None
+                elif re_end.match(l):
+                    fill = tail
+                    fill.append(l)
+                elif fill is not None:
+                    fill.append(l)
+        with path_deps.open('r', encoding='utf-8') as f:
+            s = f.read()
+            relative = path_in.relative_to(pathRoot)
+            s = s.replace('PATH_ROOT/', '../' * (len(relative.parts) - 1))
+            head.append(s)
+            head.extend(tail)
+        path_in.write_text(''.join(head), encoding='utf-8')
+    return 0
+
 
 print('Step 3:')
 print('=======')
@@ -66,6 +121,13 @@ out = updateBuild(pathRoot / 'bin' / 'build.sh',
 out = updateWeb(pathRoot / 'src' / 'index.ejs',
                 pathRoot / 'src' / 'index.ejs',
                      pathBuild / 'deps-vue.txt')
+
+out = updateTest(pathRoot / 'test' / 'import.js',
+                pathRoot / 'test' / 'import.js',
+                     pathBuild / 'deps-test-import.txt')
+
+out = updateWebTests(pathRoot / 'src/lib/eyo', pathBuild / 'deps-web-test.txt')
+
 try:
     out = updateWeb(pathRoot / 'sandbox' / 'html' / 'toolbox.html',
                     pathRoot / 'sandbox' / 'html' / 'toolbox.html',
---
+++ src/lib/eyo/block/delegate_svg_list.test.js
@@ -0,0 +1,21 @@
+var assert = chai.assert
+
+describe('Rendering', function() {
+  it('expression_stmt', function() {
+    var b1 = eYo.DelegateSvg.newBlockReady(Blockly.mainWorkspace, eYo.T3.Stmt.expression_stmt)
+    assert(b1, `MISSING assignment expression`)
+    assert(b1.eyo.comment_variant_p === eYo.Key.COMMENT, `MISSING ${b1.eyo.comment_variant_p} === ${eYo.Key.COMMENT}`)
+  })
+  it('expression_stmt(expr)', function() {
+    var b1 = eYo.DelegateSvg.newBlockReady(Blockly.mainWorkspace, eYo.T3.Stmt.expression_stmt)
+    assert(b1, `MISSING assignment expression`)
+    var b2 = eYo.DelegateSvg.newBlockReady(Blockly.mainWorkspace, eYo.T3.Expr.identifier)
+    assert(b2, `MISSING identifier`)
+    b1.eyo.variant_p = eYo.Key.EXPRESSION
+    b1.eyo.comment_variant_p = eYo.Key.NONE
+    assert(b1.eyo.variant_p === eYo.Key.EXPRESSION, `MISSING ${eYo.Key.EXPRESSION} === ${b1.eyo.variant_p}`)
+    assert(b1.eyo.comment_variant_p === eYo.Key.NONE, `MISSING ${eYo.Key.NONE}`)
+    b1.eyo.expression_s.connection.connect(b2.outputConnection)
+    assert(b1.eyo.comment_variant_p === eYo.Key.NONE, `MISSING ${eYo.Key.NONE}`)
+  })
+})
