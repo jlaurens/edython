2150feb6ba679fc88b94cdba5538f235ec177252
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-05-12 17:42:38 +0200

    Update comp.js

--- src/lib/eyo/brick/comp.js
+++ src/lib/eyo/brick/comp.js
@@ -76,7 +76,7 @@ eYo.Brick.Expr.makeSubclass('comprehension', {
     check: /** @suppress {globalThis} */ function (type) {
       // `this` is a magnet
       // we do not take the type argument into account
-      var eyo = this.b_eyo
+      var eyo = this.brick
       var b = eyo.expression_b
       if (b) {
         if (b.type === eYo.T3.Expr.key_datum || b.type === eYo.T3.Expr.identifier_annotated) {
