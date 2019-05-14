7404876aeec70df0849f72a37f684baf4ab10940
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-05-14 08:57:04 +0200

    Update proc.test.js

--- src/lib/eyo/brick/proc.test.js
+++ src/lib/eyo/brick/proc.test.js
@@ -1,9 +1,9 @@
 describe('decorator', function() {
   it(`Copy/Paste with n_ary`, function() {
     var d = eYo.Test.new_dlgt(eYo.T3.Stmt.decorator_stmt)
-    eYo.Test.dlgt(d, 'decorator_stmt')
+    eYo.Test.brick(d, 'decorator_stmt')
     d.variant_p = eYo.Key.N_ARY
-    var d = eYo.Xml.dlgtToDom(d)
+    var d = eYo.Xml.brickToDom(d)
     var dd = eYo.Test.new_dlgt(d)
     eYo.Test.same(d, dd)
     dd.dispose()
