9c061b0183e37cfe16140bed8360b61b067eb230
jlaurens <jerome.laurens@u-bourgogne.fr>
2019-12-16 08:52:48 +0100

    Update rename.py

--- bin/refactor/rename.py
+++ bin/refactor/rename.py
@@ -8,8 +8,14 @@ project_path = Path(__file__).resolve().parent.parent.parent
 src_path = project_path / 'src'
 eyo_path = project_path / 'src/lib/eyo'
 
-for p in eyo_path.glob('**/*.js'):
+for p in eyo_path.rglob('*.*'):
   name = p.name
-  if name.endwith('__model.js'):
-    q = p.with_name(name[:-10]+'__module.js')
+  if name.endswith('_module.js'):
+    q = p.with_name('module_' + name[:-10] + '.js')
+    p.rename(q)
+  elif name.endswith('_module.test.js'):
+    q = p.with_name('module_' + name[:-15]+'.test.js')
+    p.rename(q)
+  elif name.endswith('_module.test.html'):
+    q = p.with_name('module_' + name[:-17] + '.test.html')
     p.rename(q)
