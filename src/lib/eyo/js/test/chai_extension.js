dc98c800719a51217ec8d84b0d43b11bdc8cdb44
jlaurens <jerome.laurens@u-bourgogne.fr>
2020-04-22 21:55:33 +0200

    Update chai_extension.js

--- src/lib/eyo/js/test/chai_extension.js
+++ src/lib/eyo/js/test/chai_extension.js
@@ -136,95 +136,91 @@ chai.use(function (_chai, utils) {
  * Sets global tolerance and returns a function to be passed to chai.use
  * @see http://chaijs.com/guide/plugins/
  */
-eYo.test.chaiAlmost = function (standardTolerance) {
- 
-  return function (chai, utils) {
-    let Assertion = chai.Assertion
-    let flag = utils.flag
-    /**
-     * Returns a new shallow equality function to override
-     * .equal, .equals, .eq that tests 'almost' equality
-     * if both values are numbers and a 'tolerance' flag is set.
-     * Sends to deep equality check if deep flag is set
-     */
-    function overrideAssertEqual (_super) {
-      return function assertEqual (val, msg) {
-        if (msg) flag(this, 'message', msg)
 
-        var deep = flag(this, 'deep')
-        var tol = flag(this, 'tolerance')
-        
-        if (deep) {
-          return this.eql(val)
-        }
-        if (tol) {
-          var yorn = eYo.test.makeComparator(tol)(val,this._obj)
-          if (yorn !== null) {
-            this.assert(yorn,
-              `expected ${val.description} to almost equal ${this._obj.description}`,
-              `expected ${val.description} to not almost equal ${this._obj.description}`,
-              val.description,
-              this._obj.description
-            )
-            return
-          }
-        }
-        return _super.apply(this, arguments)
+chai.use(function (chai, utils) {
+  let Assertion = chai.Assertion
+  let flag = utils.flag
+  /**
+   * Returns a new shallow equality function to override
+   * .equal, .equals, .eq that tests 'almost' equality
+   * if both values are numbers and a 'tolerance' flag is set.
+   * Sends to deep equality check if deep flag is set
+   */
+  function overrideAssertEqual (_super) {
+    return function assertEqual (val, msg) {
+      if (msg) flag(this, 'message', msg)
+
+      var deep = flag(this, 'deep')
+      var tol = flag(this, 'tolerance')
+      
+      if (deep) {
+        return this.eql(val)
       }
-    }
-    /**
-     * Returns a new deep equality function to override
-     * .eql, .eqls that tests 'almost' equality if both corresponding
-     * values are numbers and tolerance flag is set
-     */
-    function overrideAssertEql (_super) {
-      return function assertEql (val, msg) {
-        if (msg) flag(this, 'message', msg)
-
-        if (eYo.geom) {
-          if (this._obj instanceof eYo.geom.AbstractPoint) {
-            return this.eqlPoint(val)
-          }
-          if (this._obj instanceof eYo.geom.AbstractRect) {
-            return this.eqlRect(val)
-          }
+      if (tol) {
+        var yorn = eYo.test.makeComparator(tol)(val,this._obj)
+        if (yorn !== null) {
+          this.assert(yorn,
+            `expected ${val.description} to almost equal ${this._obj.description}`,
+            `expected ${val.description} to not almost equal ${this._obj.description}`,
+            val.description,
+            this._obj.description
+          )
+          return
         }
-        if (eYo.span) {
-          if (this._obj instanceof eYo.span.BaseC9r) {
-            return this.eqlSpan(val)
-          }
-        }
-        return _super.apply(this, arguments)
       }
+      return _super.apply(this, arguments)
     }
-    /**
-     * .almost() method. To be used at the end of the chain like:
-     * expect(4).to.not.be.almost(5, 1.5). Simply adds tolerance flag then calls
-     * .equal. This will redirect to .eql if deep flag set
-     */
-    function method (val, tolerance = standardTolerance) {
-      flag(this, 'tolerance', tolerance)
-      return this.equal(val)
-    }
-
-    /**
-     * .almost chainable property to be used like:
-     * expect(3.99999999).to.almost.equal(4). Simply adds
-     * tolerance flag to be read by equality checking methods
-     */
-    function chainingBehavior () {
-      flag(this, 'tolerance', standardTolerance)
-    }
-
-    Assertion.addChainableMethod('almost', method, chainingBehavior)
-
-    Assertion.overwriteMethod('equal', overrideAssertEqual)
-    Assertion.overwriteMethod('equals', overrideAssertEqual)
-    Assertion.overwriteMethod('eq', overrideAssertEqual)
-
-    Assertion.overwriteMethod('eql', overrideAssertEql)
-    Assertion.overwriteMethod('eqls', overrideAssertEql)
   }
-}
+  /**
+   * Returns a new deep equality function to override
+   * .eql, .eqls that tests 'almost' equality if both corresponding
+   * values are numbers and tolerance flag is set
+   */
+  function overrideAssertEql (_super) {
+    return function assertEql (val, msg) {
+      if (msg) flag(this, 'message', msg)
 
-chai.use(eYo.test.chaiAlmost(eYo.geom && eYo.geom.EPSILON || 1e-10))
+      if (eYo.geom) {
+        if (this._obj instanceof eYo.geom.AbstractPoint) {
+          return this.eqlPoint(val)
+        }
+        if (this._obj instanceof eYo.geom.AbstractRect) {
+          return this.eqlRect(val)
+        }
+      }
+      if (eYo.span) {
+        if (this._obj instanceof eYo.span.BaseC9r) {
+          return this.eqlSpan(val)
+        }
+      }
+      return _super.apply(this, arguments)
+    }
+  }
+  /**
+   * .almost() method. To be used at the end of the chain like:
+   * expect(4).to.not.be.almost(5, 1.5). Simply adds tolerance flag then calls
+   * .equal. This will redirect to .eql if deep flag set
+   */
+  function method (val, tolerance = eYo.EPSILON) {
+    flag(this, 'tolerance', tolerance)
+    return this.equal(val)
+  }
+
+  /**
+   * .almost chainable property to be used like:
+   * expect(3.99999999).to.almost.equal(4). Simply adds
+   * tolerance flag to be read by equality checking methods
+   */
+  function chainingBehavior () {
+    flag(this, 'tolerance', eYo.EPSILON)
+  }
+
+  Assertion.addChainableMethod('almost', method, chainingBehavior)
+
+  Assertion.overwriteMethod('equal', overrideAssertEqual)
+  Assertion.overwriteMethod('equals', overrideAssertEqual)
+  Assertion.overwriteMethod('eq', overrideAssertEqual)
+
+  Assertion.overwriteMethod('eql', overrideAssertEql)
+  Assertion.overwriteMethod('eqls', overrideAssertEql)
+}())
